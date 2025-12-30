8f11b18050601b4edc3e30ef281b167c7dabe6e2
Hareesh <dhareesh205@gmail.com>
2025-12-31 01:38:23 +0530

    chore: Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/",
+  base: "/portfolio-v1/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
